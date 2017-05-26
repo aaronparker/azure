a68a38c950413fe6e594e520303f373304cb5c68
Aaron Parker <aaron@stealthpuppy.com>
2017-05-26 12:02:02 -0400

    Updating Remove-Resources

--- Remove-Resources.ps1
+++ Remove-Resources.ps1
@@ -4,3 +4,6 @@ Get-AzureRmVM |  Remove-AzureRmVM -Verbose -Force
 Get-AzureRmNetworkInterface | Remove-AzureRmNetworkInterface -Verbose -Force
 Get-AzureRmPublicIpAddress | Remove-AzureRmPublicIpAddress -Verbose -Force
 Get-AzureRmStorageAccount | Remove-AzureRmStorageAccount -Verbose -Force
+Get-AzureRmDisk | Remove-AzureRmDisk -Verbose -Force
+Get-AzureRmVirtualNetwork | Remove-AzureRmVirtualNetwork -Verbose -Force
+Get-AzureRmNetworkSecurityGroup | Remove-AzureRmNetworkSecurityGroup -Verbose -Force
