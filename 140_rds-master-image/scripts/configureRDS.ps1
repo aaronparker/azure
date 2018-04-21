573f490ad91a755ba8130df4a88c1d0ca42140cf
Aaron Parker <aaron@stealthpuppy.com>
2018-04-22 01:03:17 +1000

    Update script

--- 140_rds-master-image/scripts/configureRDS.ps1
+++ 140_rds-master-image/scripts/configureRDS.ps1
@@ -7,9 +7,21 @@ This needs to be coupled with the GPO to enable autoWorkplaceJoin after the VMs 
 Set-ItemProperty -Path HKLM:\Software\Policies\Microsoft\Windows\WorkplaceJoin -Name autoWorkplaceJoin -Value 0
 
 # Add / Remove roles via DSC
-# Uninstall-WindowsFeature -Name BitLocker, EnhancedStorage, PowerShell-ISE
-# Add-WindowsFeature -Name RDS-RD-Server, Server-Media-Foundation, 'Search-Service', NET-Framework-Core
+Uninstall-WindowsFeature -Name BitLocker, EnhancedStorage, PowerShell-ISE
+Add-WindowsFeature -Name RDS-RD-Server, Server-Media-Foundation, 'Search-Service', NET-Framework-Core
 
 Set-Service Audiosrv -StartupType Automatic
+
+If (Get-PSRepository | Where-Object { $_.Name -eq "PSGallery" -and $_.InstallationPolicy -ne "Trusted" }) {
+    Write-Verbose "Trusting the repository: PSGallery"
+    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted -Force
+}
+
+Install-Module VcRedist
+
+$VcPath = "$env:SystemDrive\Apps\VcRedist"
+New-Item -Path $VcPath -ItemType Directory
+Get-VcList | Get-VcRedist -Path $VcPath
+Get-VcList | Install-VcRedist -Path $VcPath
 
 Stop-Transript
