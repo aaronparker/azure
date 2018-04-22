ad0aa4b9a755934082bdd8074aef6c0ddda5b8cd
Aaron Parker <aaron@stealthpuppy.com>
2018-04-22 14:27:53 +1000

    Adding Office 365 ProPlus

--- 140_rds-master-image/scripts/configureRDS.ps1
+++ 140_rds-master-image/scripts/configureRDS.ps1
@@ -4,16 +4,18 @@ Param (
     [String] $Log = "$env:SystemRoot\Temp\azureDeploy.log"
 )
 
-If ($VerbosePreference -ne [System.Management.Automation.ActionPreference]::SilentlyContinue) {
+If ($VerbosePreference -ne [System.Management.Automation.ActionPreference]::Continue) {
     $VerbosePreference = "Continue"
 }
 
 # Start logging
 Start-Transcript -Path $Log
 
-<# Disable autoWorkplaceJoin
-This setting is important to block the master image from registering with Azure AD.
-This needs to be coupled with the GPO to enable autoWorkplaceJoin after the VMs are provisioned to whatever worker OU exists. #>
+<#
+    Disable autoWorkplaceJoin
+    Block the master image from registering with Azure AD.
+    Enable autoWorkplaceJoin after the VMs are provisioned via GPO.
+#>
 Set-ItemProperty -Path HKLM:\Software\Policies\Microsoft\Windows\WorkplaceJoin -Name autoWorkplaceJoin -Value 0
 
 # Add / Remove roles
@@ -27,7 +29,7 @@ Set-Service Audiosrv -StartupType Automatic
 If (Get-PSRepository | Where-Object { $_.Name -eq "PSGallery" -and $_.InstallationPolicy -ne "Trusted" }) {
     Write-Verbose "Trusting the repository: PSGallery"
     Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted -Verbose
+    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 }
 
 # Install the VcRedist module
