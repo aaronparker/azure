82ebcc1c623992847b9be498cc419a63a123bdf9
Aaron Parker <aaron@stealthpuppy.com>
2018-05-10 22:40:29 -0700

    RDS script updates

--- scripts/rds/configureRDS.ps1
+++ scripts/rds/configureRDS.ps1
@@ -1,5 +1,5 @@
 <# 
-    .SYSOPSIS
+    .SYNOPSIS
         Customise a Windows Server image for use as an RDS/XenApp VM in Azure.
         Installs Office 365 ProPlus, Adobe Reader DC, Visual C++ Redistributables. Installs applications from a network path specified in AppShare.
         Sets regional settings, installs Windows Updates, configures the default profile.
@@ -7,96 +7,88 @@
 #>
 [CmdletBinding()]
 Param (
-    [Parameter()]
-    [String] $Log = "$env:SystemRoot\Logs\AzureArmCustomDeploy.log",
-
-    [Parameter()]
-    [String] $Target = "$env:SystemDrive\Apps",
-
-    [Parameter()]
-    [String] $User,
-
-    [Parameter()]
-    [String] $Pass,
-
-    [Parameter()]
-    [String] $AppShare
+    [Parameter()] $Log = "$env:SystemRoot\Logs\AzureArmCustomDeploy.log",
+    [Parameter()] $Target = "$env:SystemDrive\Apps",
+    [Parameter()] $User,
+    [Parameter()] $Pass,
+    [Parameter()] $AppShare,
+    [Parameter()] $VerbosePreference = "Continue"
 )
 
-# Start logging; Set $VerbosePreference so full details are sent to the log
-$VerbosePreference = "Continue"
+# Start logging
 Start-Transcript -Path $Log
-New-Item -Path $Target -ItemType Directory
 
+# If local path for script doesn't exist, create it
+If (!(Test-Path $Target)) { New-Item -Path $Target -Type Directory -Force }
 
 #region Configure system
-# Block the master image from registering with Azure AD; Enable autoWorkplaceJoin after the VMs are provisioned via GPO.
-Set-ItemProperty -Path HKLM:\Software\Policies\Microsoft\Windows\WorkplaceJoin -Name autoWorkplaceJoin -Value 0
+    # Block the master image from registering with Azure AD; Enable autoWorkplaceJoin after the VMs are provisioned via GPO.
+    Set-ItemProperty -Path HKLM:\Software\Policies\Microsoft\Windows\WorkplaceJoin -Name autoWorkplaceJoin -Value 0
 
-# Regional settings - set to en-AU / Australia
-Import-Module International
-Set-WinHomeLocation -GeoId 12
-Set-WinSystemLocale -SystemLocale en-AU
-Set-TimeZone -Id "AUS Eastern Standard Time" -Verbose
-$url = "https://raw.githubusercontent.com/aaronparker/build-azure-lab/master/scripts/common/language.xml"
-Start-BitsTransfer -Source $url -Destination "$Target\$(Split-Path $url -Leaf)"
-& $env:SystemRoot\System32\control.exe "intl.cpl,,/f:`"$Target\language.xml`""
+    # Regional settings - set to en-AU / Australia
+    Import-Module International
+    Set-WinHomeLocation -GeoId 12
+    Set-WinSystemLocale -SystemLocale en-AU
+    Set-TimeZone -Id "AUS Eastern Standard Time" -Verbose
+    $url = "https://raw.githubusercontent.com/aaronparker/build-azure-lab/master/scripts/common/language.xml"
+    Start-BitsTransfer -Source $url -Destination "$Target\$(Split-Path $url -Leaf)"
+    & $env:SystemRoot\System32\control.exe "intl.cpl,,/f:`"$Target\language.xml`""
 
-# Add / Remove roles (requires reboot at end of deployment)
-Disable-WindowsOptionalFeature -Online -FeatureName "Printing-XPSServices-Features", "WindowsMediaPlayer" -NoRestart -WarningAction SilentlyContinue
-Uninstall-WindowsFeature -Name BitLocker, EnhancedStorage, PowerShell-ISE
-Add-WindowsFeature -Name RDS-RD-Server, Server-Media-Foundation, 'Search-Service', NET-Framework-Core
+    # Add / Remove roles (requires reboot at end of deployment)
+    Disable-WindowsOptionalFeature -Online -FeatureName "Printing-XPSServices-Features", "WindowsMediaPlayer" -NoRestart -WarningAction SilentlyContinue
+    Uninstall-WindowsFeature -Name BitLocker, EnhancedStorage, PowerShell-ISE
+    Add-WindowsFeature -Name RDS-RD-Server, Server-Media-Foundation, 'Search-Service', NET-Framework-Core
 
-# Configure services
-Set-Service Audiosrv -StartupType Automatic
-Set-Service WSearch -StartupType Automatic
+    # Configure services
+    Set-Service Audiosrv -StartupType Automatic
+    Set-Service WSearch -StartupType Automatic
 #endregion
 
 
 #region Applications - source Internet
-# Trust the PSGaller for installing modules
-If (Get-PSRepository | Where-Object { $_.Name -eq "PSGallery" -and $_.InstallationPolicy -ne "Trusted" }) {
-    Write-Verbose "Trusting the repository: PSGallery"
-    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-}
+    # Trust the PSGallery for installing modules
+    If (Get-PSRepository | Where-Object { $_.Name -eq "PSGallery" -and $_.InstallationPolicy -ne "Trusted" }) {
+        Write-Verbose "Trusting the repository: PSGallery"
+        Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+        Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+    }
 
-# Install the VcRedist module and VcRedists
-# https://docs.stealthpuppy.com/vcredist/
-Install-Module VcRedist
-$Dest = "$Target\VcRedist"
-If (!(Test-Path $Dest)) { New-Item -Path $Dest -ItemType Directory }
-$VcList = Get-VcList | Get-VcRedist -Path $Dest
-Install-VcRedist -VcList $VcList -Path $Dest
+    # Install the VcRedist module and VcRedists
+    # https://docs.stealthpuppy.com/vcredist/
+    Install-Module VcRedist
+    $Dest = "$Target\VcRedist"
+    If (!(Test-Path $Dest)) { New-Item -Path $Dest -ItemType Directory }
+    $VcList = Get-VcList | Get-VcRedist -Path $Dest
+    Install-VcRedist -VcList $VcList -Path $Dest
 
-# Install Office 365 ProPlus; manage installed options in configurationRDS.xml
-$Dest = "$Target\Office"
-If (!(Test-Path $Dest)) { New-Item -Path $Dest -ItemType Directory }
-$url = "https://raw.githubusercontent.com/aaronparker/build-azure-lab/master/scripts/rds/Office.zip"
-Start-BitsTransfer -Source $url -Destination "$Dest\$(Split-Path $url -Leaf)"
-Expand-Archive -Path "$Dest\$(Split-Path $url -Leaf)" -DestinationPath "$Dest"
-# Expand-Archive -Path "$PSScriptRoot\Office.zip" -DestinationPath "$Dest"
-Start-Process -FilePath "$Dest\setup.exe" -ArgumentList "/configure $Dest\configurationRDS.xml" -Wait
+    # Install Office 365 ProPlus; manage installed options in configurationRDS.xml
+    $Dest = "$Target\Office"
+    If (!(Test-Path $Dest)) { New-Item -Path $Dest -ItemType Directory }
+    $url = "https://raw.githubusercontent.com/aaronparker/build-azure-lab/master/scripts/rds/Office.zip"
+    Start-BitsTransfer -Source $url -Destination "$Dest\$(Split-Path $url -Leaf)"
+    Expand-Archive -Path "$Dest\$(Split-Path $url -Leaf)" -DestinationPath "$Dest"
+    # Expand-Archive -Path "$PSScriptRoot\Office.zip" -DestinationPath "$Dest"
+    Start-Process -FilePath "$Dest\setup.exe" -ArgumentList "/configure $Dest\configurationRDS.xml" -Wait
 
-# Install Adobe Reader DC
-# Enforce settings with GPO: https://www.adobe.com/devnet-docs/acrobatetk/tools/AdminGuide/gpo.html
-$Dest = "$Target\Reader"
-New-Item -Path $Dest -ItemType Directory
-$url = "http://ardownload.adobe.com/pub/adobe/reader/win/AcrobatDC/1801120035/AcroRdrDC1801120035_en_US.exe"
-Start-BitsTransfer -Source $url -Destination "$Dest\$(Split-Path $url -Leaf)"
-$Arguments = "-sfx_nu /sALL /msi EULA_ACCEPT=YES " + `
-    "ENABLE_CHROMEEXT=0 " + `
-    "DISABLE_BROWSER_INTEGRATION=1 " + `
-    "ENABLE_OPTIMIZATION=YES " + `
-    "ADD_THUMBNAILPREVIEW=0 " + `
-    "DISABLEDESKTOPSHORTCUT=1 " + `
-    "UPDATE_MODE=0 " + `
-    "DISABLE_ARM_SERVICE_INSTALL=1"
-Start-Process -FilePath "$Dest\$(Split-Path $url -Leaf)" -ArgumentList $Arguments -Wait
-$url = "http://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1801120038/AcroRdrDCUpd1801120038.msp"
-Start-BitsTransfer -Source $url -Destination "$Dest\$(Split-Path $url -Leaf)"
-Start-Process -FilePath "$env:SystemRoot\System32\msiexec" -ArgumentList "/quiet /update $Dest\$(Split-Path $url -Leaf)" -Wait
-Get-Service -Name AdobeARMservice | Set-Service -StartupType Disabled
+    # Install Adobe Reader DC
+    # Enforce settings with GPO: https://www.adobe.com/devnet-docs/acrobatetk/tools/AdminGuide/gpo.html
+    $Dest = "$Target\Reader"
+    New-Item -Path $Dest -ItemType Directory
+    $url = "http://ardownload.adobe.com/pub/adobe/reader/win/AcrobatDC/1801120035/AcroRdrDC1801120035_en_US.exe"
+    Start-BitsTransfer -Source $url -Destination "$Dest\$(Split-Path $url -Leaf)"
+    $Arguments = "-sfx_nu /sALL /msi EULA_ACCEPT=YES " + `
+        "ENABLE_CHROMEEXT=0 " + `
+        "DISABLE_BROWSER_INTEGRATION=1 " + `
+        "ENABLE_OPTIMIZATION=YES " + `
+        "ADD_THUMBNAILPREVIEW=0 " + `
+        "DISABLEDESKTOPSHORTCUT=1 " + `
+        "UPDATE_MODE=0 " + `
+        "DISABLE_ARM_SERVICE_INSTALL=1"
+    Start-Process -FilePath "$Dest\$(Split-Path $url -Leaf)" -ArgumentList $Arguments -Wait
+    $url = "http://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1801120038/AcroRdrDCUpd1801120038.msp"
+    Start-BitsTransfer -Source $url -Destination "$Dest\$(Split-Path $url -Leaf)"
+    Start-Process -FilePath "$env:SystemRoot\System32\msiexec" -ArgumentList "/quiet /update $Dest\$(Split-Path $url -Leaf)" -Wait
+    Get-Service -Name AdobeARMservice | Set-Service -StartupType Disabled
 #endregion
 
 
@@ -129,71 +121,71 @@ If ($AppShare) {
 
 
 #region Customisations
-# DisableIEEnhancedSecurity 
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}" -Name "IsInstalled" -Type DWORD -Value 0
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}" -Name "IsInstalled" -Type DWORD -Value 0
+    # DisableIEEnhancedSecurity 
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}" -Name "IsInstalled" -Type DWORD -Value 0
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}" -Name "IsInstalled" -Type DWORD -Value 0
 
-# HideServerManagerOnLogin 
-If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Server\ServerManager")) {
-    New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Server\ServerManager" -Force
-}
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Server\ServerManager" -Name "DoNotOpenAtLogon" -Type DWORD -Value 1
+    # HideServerManagerOnLogin 
+    If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Server\ServerManager")) {
+        New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Server\ServerManager" -Force
+    }
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Server\ServerManager" -Name "DoNotOpenAtLogon" -Type DWORD -Value 1
 
-# EnableSmartScreen
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\System" -Name "EnableSmartScreen" -Type DWORD -Value 2
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\MicrosoftEdge\PhishingFilter" -Name "EnabledV9" -Type DWORD -Value 1
+    # EnableSmartScreen
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\System" -Name "EnableSmartScreen" -Type DWORD -Value 2
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\MicrosoftEdge\PhishingFilter" -Name "EnabledV9" -Type DWORD -Value 1
 
-# DisableAutorun
-If (!(Test-Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer")) {
-    New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer"
-}
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "NoDriveTypeAutoRun" -Type DWORD -Value 255
+    # DisableAutorun
+    If (!(Test-Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer")) {
+        New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer"
+    }
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "NoDriveTypeAutoRun" -Type DWORD -Value 255
 
-# Default Profile etc.
-$Dest = "$Target\Customise"
-If (!(Test-Path $Dest)) { New-Item -Path $Dest -ItemType Directory }
-$url = "https://raw.githubusercontent.com/aaronparker/build-azure-lab/master/scripts/rds/Customise.zip"
-Start-BitsTransfer -Source $url -Destination "$Dest\$(Split-Path $url -Leaf)"
-Expand-Archive -Path "$Dest\$(Split-Path $url -Leaf)"  -DestinationPath "$Dest"
-# Expand-Archive -Path "$PSScriptRoot\Customise.zip" -DestinationPath "$Dest"
-Push-Location $Dest
-Get-ChildItem -Path $Dest -Filter *.ps1 | ForEach-Object { & $_.FullName }
-Pop-Location
+    # Default Profile etc.
+    $Dest = "$Target\Customise"
+    If (!(Test-Path $Dest)) { New-Item -Path $Dest -ItemType Directory }
+    $url = "https://raw.githubusercontent.com/aaronparker/build-azure-lab/master/scripts/rds/Customise.zip"
+    Start-BitsTransfer -Source $url -Destination "$Dest\$(Split-Path $url -Leaf)"
+    Expand-Archive -Path "$Dest\$(Split-Path $url -Leaf)"  -DestinationPath "$Dest"
+    # Expand-Archive -Path "$PSScriptRoot\Customise.zip" -DestinationPath "$Dest"
+    Push-Location $Dest
+    Get-ChildItem -Path $Dest -Filter *.ps1 | ForEach-Object { & $_.FullName }
+    Pop-Location
 #endregion
 
 
 #region Optimisations
-# Windows Updates
-Install-Module PSWindowsUpdate
-Add-WUServiceManager -ServiceID "7971f918-a847-4430-9279-4a52d1efe18d" -Confirm:$False
-Get-WUInstall -MicrosoftUpdate -Confirm:$False -IgnoreReboot -AcceptAll -Install
+    # Windows Updates
+    Install-Module PSWindowsUpdate
+    Add-WUServiceManager -ServiceID "7971f918-a847-4430-9279-4a52d1efe18d" -Confirm:$False
+    Get-WUInstall -MicrosoftUpdate -Confirm:$False -IgnoreReboot -AcceptAll -Install
 
-<#
-# Run Windows Defender quick scan; Running via BISF doesn't exit
-Start-Process -FilePath "$env:ProgramFiles\Windows Defender\MpCmdRun.exe" -ArgumentList "-SignatureUpdate -MMPC" -Wait
-Start-Process -FilePath "$env:ProgramFiles\Windows Defender\MpCmdRun.exe" -ArgumentList "-Scan -ScanType 1" -Wait
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\RemovalTools\MRT" -Name "GUID" -Value ""
+    <#
+    # Run Windows Defender quick scan; Running via BISF doesn't exit
+    Start-Process -FilePath "$env:ProgramFiles\Windows Defender\MpCmdRun.exe" -ArgumentList "-SignatureUpdate -MMPC" -Wait
+    Start-Process -FilePath "$env:ProgramFiles\Windows Defender\MpCmdRun.exe" -ArgumentList "-Scan -ScanType 1" -Wait
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\RemovalTools\MRT" -Name "GUID" -Value ""
 
-# Citrix Optimizer
-$Dest = "$Target\CitrixOptimizer"
-If (!(Test-Path $Dest)) { New-Item -Path $Dest -ItemType Directory }
-$url = "https://raw.githubusercontent.com/aaronparker/build-azure-lab/master/scripts/rds/CitrixOptimizer.zip"
-Start-BitsTransfer -Source $url -Destination "$Dest\$(Split-Path $url -Leaf)"
-Expand-Archive -Path "$Dest\$(Split-Path $url -Leaf)"  -DestinationPath "$Dest"
-& "$Dest\CtxOptimizerEngine.ps1" `
-    -Source "$Dest\Templates\WindowsServer2016-WindowsDefender-Azure.xml" `
-    -Mode execute -OutputHtml "$Dest\CitrixOptimizer.html"
+    # Citrix Optimizer
+    $Dest = "$Target\CitrixOptimizer"
+    If (!(Test-Path $Dest)) { New-Item -Path $Dest -ItemType Directory }
+    $url = "https://raw.githubusercontent.com/aaronparker/build-azure-lab/master/scripts/rds/CitrixOptimizer.zip"
+    Start-BitsTransfer -Source $url -Destination "$Dest\$(Split-Path $url -Leaf)"
+    Expand-Archive -Path "$Dest\$(Split-Path $url -Leaf)"  -DestinationPath "$Dest"
+    & "$Dest\CtxOptimizerEngine.ps1" `
+        -Source "$Dest\Templates\WindowsServer2016-WindowsDefender-Azure.xml" `
+        -Mode execute -OutputHtml "$Dest\CitrixOptimizer.html"
 
-# BIS-F
-$Dest = "$Target\BISF"
-If (!(Test-Path $Dest)) { New-Item -Path $Dest -ItemType Directory }
-$url = "https://raw.githubusercontent.com/aaronparker/build-azure-lab/master/scripts/rds/bisf-6.1.0.zip"
-Start-BitsTransfer -Source $url -Destination "$Dest\$(Split-Path $url -Leaf)"
-Expand-Archive -Path "$Dest\$(Split-Path $url -Leaf)"  -DestinationPath "$Dest"
-Start-Process -FilePath "$Dest\setup-BIS-F-6.1.0_build01.100.exe" -ArgumentList "/SILENT"
-Remove-Item -Path "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Base Image Script Framework (BIS-F).lnk" -Force
-Copy-Item -Path "$Dest\*.xml" -Destination "${env:ProgramFiles(x86)}\Base Image Script Framework (BIS-F)"
-& "${env:ProgramFiles(x86)}\Base Image Script Framework (BIS-F)\Framework\PrepBISF_Start.ps1"
+    # BIS-F
+    $Dest = "$Target\BISF"
+    If (!(Test-Path $Dest)) { New-Item -Path $Dest -ItemType Directory }
+    $url = "https://raw.githubusercontent.com/aaronparker/build-azure-lab/master/scripts/rds/bisf-6.1.0.zip"
+    Start-BitsTransfer -Source $url -Destination "$Dest\$(Split-Path $url -Leaf)"
+    Expand-Archive -Path "$Dest\$(Split-Path $url -Leaf)"  -DestinationPath "$Dest"
+    Start-Process -FilePath "$Dest\setup-BIS-F-6.1.0_build01.100.exe" -ArgumentList "/SILENT"
+    Remove-Item -Path "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Base Image Script Framework (BIS-F).lnk" -Force
+    Copy-Item -Path "$Dest\*.xml" -Destination "${env:ProgramFiles(x86)}\Base Image Script Framework (BIS-F)"
+    & "${env:ProgramFiles(x86)}\Base Image Script Framework (BIS-F)\Framework\PrepBISF_Start.ps1"
 #endregion
 
 
