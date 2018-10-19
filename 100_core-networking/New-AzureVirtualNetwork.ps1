b0594a12361fcd9e75c8719c11d32bfe046d597d
Aaron Parker <aaron@stealthpuppy.com>
2018-10-19 11:14:56 +1100

    Update 100_core-networking/New-AzureVirtualNetwork.ps1

--- 100_core-networking/New-AzureVirtualNetwork.ps1
+++ 100_core-networking/New-AzureVirtualNetwork.ps1
@@ -1,17 +1,20 @@
-# Australia Southeast
-$locations = @("AustraliaSoutheast")
-
-# Network space
-$addressSpace = "10.0.0.0/16"
-$gatewayAddress = "10.0.0.0/27"
-$jumpAddress = "10.0.1.0/27"
-$authAddress = "10.0.2.0/24"
-$dataAddress = "10.0.3.0/24"
-$appAddress = "10.0.4.0/24"
-$rdsAddress = "10.0.5.0/24"
-$dmzAddress = "10.0.254.0/24"
-
-ForEach ($location in $locations) {
+# Properties
+Function New-AzureVirtualNetwork {
+    <#
+        Create a new virtual network
+    #>
+    [CmdletBinding()]
+    Param (
+        [Parameter()] $Location        = 'AustraliaSoutheast',
+        [Parameter()] $AddressSpace    = '10.0.0.0/16',
+        [Parameter()] $GatewayAddress  = '10.0.0.0/27',
+        [Parameter()] $JumpAddress     = '10.0.1.0/27',
+        [Parameter()] $AuthAddress     = '10.0.2.0/24',
+        [Parameter()] $DataAddress     = '10.0.3.0/24',
+        [Parameter()] $AppAddress      = '10.0.4.0/24',
+        [Parameter()] $RdsAddress      = '10.0.5.0/24',
+        [Parameter()] $DmzAddress      = '10.0.254.0/24'
+    )
 
     # Common variables
     $gateway = "GatewaySubnet"
@@ -53,6 +56,44 @@ ForEach ($location in $locations) {
         -Subnet $gatewaySubnetCfg, $appSubnetCfg, $authSubnetCfg, $dataSubnetCfg, $dmzSubnetCfg, $jumpSubnetCfg, $rdsSubnetCfg
 
     $output += $vnet
+    Write-Output $output
 }
 
-Write-Output $output
+
+
+$network1 = @{
+    Location        = 'AustraliaSoutheast'
+    AddressSpace    = '10.0.0.0/16'
+    GatewayAddress  = '10.0.0.0/27'
+    JumpAddress     = '10.0.1.0/27'
+    AuthAddress     = '10.0.2.0/24'
+    DataAddress     = '10.0.3.0/24'
+    AppAddress      = '10.0.4.0/24'
+    RdsAddress      = '10.0.5.0/24'
+    DmzAddress      = '10.0.254.0/24'
+}
+$network2 = @{
+    Location        = 'AustraliaEast'
+    AddressSpace    = '10.1.0.0/16'
+    GatewayAddress  = '10.1.0.0/27'
+    JumpAddress     = '10.1.1.0/27'
+    AuthAddress     = '10.1.2.0/24'
+    DataAddress     = '10.1.3.0/24'
+    AppAddress      = '10.1.4.0/24'
+    RdsAddress      = '10.1.5.0/24'
+    DmzAddress      = '10.1.254.0/24'
+}
+
+
+
+$sharedKey = (Get-AzureKeyVaultSecret -VaultName $keyVaultName -Name vnetGatewaySharedKey).SecretValueText
+
+# Virtual network gateway connections
+$localNetworkGateway = New-AzureRmLocalNetworkGateway -Name $localNetName -ResourceGroupName $resourceGroup -Location $location `
+    -GatewayIpAddress $GatewayIpAddress -AddressPrefix $addressPrefix
+
+$virtualNetworkGateway = Get-AzureRmVirtualNetworkGateway -ResourceGroupName $resourceGroup -Name "$locPrefix-vnetgateway"
+
+New-AzureRmVirtualNetworkGatewayConnection -Name $connectionName -ConnectionType IPsec -ResourceGroupName $resourceGroup `
+    -Location $location -VirtualNetworkGateway1 $virtualNetworkGateway -LocalNetworkGateway2 $localNetworkGateway `
+    -RoutingWeight $routingWeight -SharedKey $sharedKey
