c5c3d9d411328726e2fe2429b7095bdff575cca4
Aaron Parker <aaron@stealthpuppy.com>
2020-01-10 19:48:36 +1100

    CoreApp update

--- rds-packer/Rds-CoreApps.ps1
+++ rds-packer/Rds-CoreApps.ps1
@@ -16,7 +16,7 @@ Param (
 
 #region Functions
 Function Set-Repository {
-    # Trust the PSGallery for installing modules
+    # Trust the PSGallery for modules
     If (Get-PSRepository | Where-Object { $_.Name -eq "PSGallery" -and $_.InstallationPolicy -ne "Trusted" }) {
         Write-Verbose "Trusting the repository: PSGallery"
         Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
@@ -30,6 +30,7 @@ Function Install-CoreApps {
     # https://docs.stealthpuppy.com/vcredist/
     Install-Module -Name VcRedist -AllowClobber
 
+    Write-Host "========== Microsoft Visual C++ Redistributables"
     $Dest = "$Target\VcRedist"
     If (!(Test-Path $Dest)) { New-Item -Path $Dest -ItemType Directory -Force -ErrorAction SilentlyContinue }
     $VcList = Get-VcList
@@ -41,7 +42,7 @@ Function Install-CoreApps {
     Install-Module -Name Evergreen -AllowClobber
 
     #region FSLogix Apps
-    Write-Host "Installing FSLogix agent"
+    Write-Host "========== Microsoft FSLogix agent"
     $Dest = "$Target\FSLogix"
     If (!(Test-Path $Dest)) { New-Item -Path $Dest -ItemType Directory -Force -ErrorAction SilentlyContinue }
 
@@ -55,18 +56,21 @@ Function Install-CoreApps {
 
 
     #region Edge
-    Write-Host "Installing Microsoft Edge"
+    Write-Host "========== Microsoft Edge"
     $Dest = "$Target\Edge"
     If (!(Test-Path $Dest)) { New-Item -Path $Dest -ItemType Directory -Force -ErrorAction SilentlyContinue }
 
+    Write-Host "=============== Downloading Microsoft Edge"
     $url = "http://dl.delivery.mp.microsoft.com/filestreamingservice/files/89e511fc-33dd-4869-b781-81b4264b3e1e/MicrosoftEdgeBetaEnterpriseX64.msi"
     Invoke-WebRequest -Uri $url -OutFile "$Dest\$(Split-Path -Path $url -Leaf)" -UseBasicParsing
+
+    Write-Host "=============== Installing Microsoft Edge"
     Start-Process -FilePath "$env:SystemRoot\System32\msiexec.exe" -ArgumentList "/package $Dest\$(Split-Path -Path $url -Leaf) /quiet /norestart" -Wait
     #endregion
 
 
     #region Office
-    Write-Host "Installing Microsoft Office"
+    Write-Host "========== Microsoft Office"
     # Install Office 365 ProPlus; manage installed options in configurationRDS.xml
     $Dest = "$Target\Office"
     If (!(Test-Path $Dest)) { New-Item -Path $Dest -ItemType Directory -Force -ErrorAction SilentlyContinue }
@@ -77,18 +81,24 @@ Function Install-CoreApps {
 
     $Office = Get-MicrosoftOffice
     Invoke-WebRequest -Uri $Office[0].URI -OutFile "$Dest\$(Split-Path -Path $Office[0].URI -Leaf)"
-    Start-Process -FilePath "$Dest\$(Split-Path -Path $Office[0].URI -Leaf)" -ArgumentList "/configure $Dest\$(Split-Path -Path $url -Leaf)" -Wait
+    Push-Location -Path $Dest
+    Write-Host "=============== Downloading Microsoft Office"
+    Start-Process -FilePath "$Dest\$(Split-Path -Path $Office[0].URI -Leaf)" -ArgumentList "/download $Dest\$(Split-Path -Path $url -Leaf)" -Wait
+    Write-Host "=============== Installing Microsoft Office"
+    # Start-Process -FilePath "$Dest\$(Split-Path -Path $Office[0].URI -Leaf)" -ArgumentList "/configure $Dest\$(Split-Path -Path $url -Leaf)" -Wait
+    Pop-Location
     #endregion
 
 
     #region Reader
     # Install Adobe Reader DC
     # Enforce settings with GPO: https://www.adobe.com/devnet-docs/acrobatetk/tools/AdminGuide/gpo.html
-    Write-Host "Installing Adobe Acrobat Reader DC"
+    Write-Host "========== Adobe Acrobat Reader DC"
     $Dest = "$Target\Reader"
     If (!(Test-Path $Dest)) { New-Item -Path $Dest -ItemType Directory -Force -ErrorAction SilentlyContinue }
 
     # Download Reader installer and updater
+    Write-Host "=============== Downloading Reader"
     $Reader = Get-AdobeAcrobatReaderDC | Where-Object { $_.Platform -eq "Windows" -and ($_.Language -eq "English" -or $_.Language -eq "Neutral") }
     ForEach ($File in $Reader) {
         Invoke-WebRequest -Uri $File.Uri -OutFile (Join-Path -Path $Dest -ChildPath (Split-Path -Path $File.Uri -Leaf)) -UseBasicParsing
@@ -98,15 +108,18 @@ Function Install-CoreApps {
     $res = Export-EvergreenFunctionStrings -AppName "AdobeAcrobatReaderDC"
 
     # Install Adobe Reader
+    Write-Host "=============== Installing Reader"
     $exe = Get-ChildItem -Path $Dest -Filter "*.exe"
     Start-Process -FilePath $exe.FullName -ArgumentList $res.Install.Virtual.Arguments -Wait
 
     # Run post install actions
+    Write-Host "=============== Post install configuration Reader"
     ForEach ($command in $res.Install.Virtual.PostInstall) {
         Invoke-Command -ScriptBlock ($executioncontext.invokecommand.NewScriptBlock($command))
     }
 
     # Update Adobe Reader
+    Write-Host "=============== Update Reader"
     $msp = Get-ChildItem -Path $Dest -Filter "*.msp"
     Start-Process -FilePath "$env:SystemRoot\System32\msiexec.exe" -ArgumentList "/update $($msp.FullName) /quiet" -Wait
     #endregion
@@ -115,7 +128,7 @@ Function Install-CoreApps {
 
 #region Script logic
 # Start logging
-Write-Host "Running: $($MyInvocation.MyCommand)."
+Write-Host "========== Running: $($MyInvocation.MyCommand)."
 Start-Transcript -Path $Log -Append
 
 # If local path for script doesn't exist, create it
@@ -130,5 +143,5 @@ Install-CoreApps
 
 # Stop Logging
 Stop-Transcript
-Write-Host "Complete: $($MyInvocation.MyCommand)."
+Write-Host "========== Complete: $($MyInvocation.MyCommand)."
 #endregion
