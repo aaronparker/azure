9dfdaad20996b336e73e898c93552e608d851b6d
Aaron Parker <aaron@stealthpuppy.com>
2020-02-04 18:35:29 +1100

    Update Invoke-Packer

--- packer/Invoke-Packer.ps1
+++ packer/Invoke-Packer.ps1
@@ -19,15 +19,23 @@ param(
     [System.String] $KeyVault = "insentrawvd"
 )
 
-# Trust the PSGallery for installing modules
-If (Get-PSRepository | Where-Object { $_.Name -eq "PSGallery" -and $_.InstallationPolicy -ne "Trusted" }) {
-    Write-Verbose "Trusting the repository: PSGallery"
-    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-}
+# Get elevation status
+[System.Boolean] $Elevated = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator") 
 
-# Install the Az module
-Install-Module -Name Az -AllowClobber
+If ($Elevated) {
+    # Trust the PSGallery for installing modules
+    If (Get-PSRepository | Where-Object { $_.Name -eq "PSGallery" -and $_.InstallationPolicy -ne "Trusted" }) {
+        Write-Verbose "Trusting the repository: PSGallery"
+        Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+        Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+    }
+
+    # Install the Az module
+    Install-Module -Name Az -AllowClobber
+}
+Else {
+    Write-Host "Not running elevated. Check modules are installed." -ForegroundColor Cyan
+}
 
 # Test if logged in and get the subscription
 If ($Null -eq (Get-AzAccount)) { Connect-AzAccount }
@@ -45,11 +53,19 @@ If ($Null -eq $AppId) {
 $date = Get-Date
 $dateFormat = "$($date.Day)$($date.Month)$($date.Year)"
 
-# Install the Windows Update Packer plugin via Chocolatey; requires 0.9.0 or above
-# https://github.com/rgl/packer-provisioner-windows-update
-Set-ExecutionPolicy Bypass -Scope Process -Force
-Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install packer-provisioner-windows-update -yes
+# Install the Windows Update Packer plugin, https://github.com/rgl/packer-provisioner-windows-update
+$url = "https://github.com/rgl/packer-provisioner-windows-update/releases/download/v0.9.0/packer-provisioner-windows-update-windows.zip"
+$zip = "$env:Temp\packer-provisioner-windows-update-windows.zip"
+$target = "$env:AppData\packer.d\plugins"
+$exe = "packer-provisioner-windows-update.exe"
+If (Test-Path -Path (Join-Path -Path $target -ChildPath $exe)) {
+    Write-Host "Windows Update Packer plugin exists" -ForegroundColor Cyan
+}
+Else {
+    Invoke-WebRequest -Uri $url -OutFile $zip -UseBasicParsing
+    Expand-Archive -Path $zip -DestinationPath $target
+    Remove-Item -Path $zip
+}
 
 # Replace strings
 (Get-Content $Template).replace("<clientid>", $AppId) | Set-Content $Template
@@ -59,9 +75,17 @@ choco install packer-provisioner-windows-update -yes
 (Get-Content $Template).replace("<resourcegroup>", $ResourceGroup) | Set-Content $Template
 (Get-Content $Template).replace("<imagename>", "$ImageName-$dateFormat") | Set-Content $Template
 
+# Output strings
+Write-Host "AppId: $AppId"
+Write-Host "Secret: $Secret"
+Write-Host "Subscription: $($sub.Id)"
+Write-Host "Subscription: $($sub.TenantId)"
+Write-Host "Resource group: $ResourceGroup"
+Write-Host "Image name: $ImageName-$dateFormat"
+Write-Host "Template: $Template"
+
 # Validate template
 Start-Process -FilePath ".\Packer.exe" -ArgumentList "validate $Template" -Wait -NoNewWindow
 
 # Run Packer
-.\packer.exe validate $ImageName
-# .\packer.exe build -force -on-error=ask -timestamp-ui .\WindowsServer2019RDS.json
+# .\packer.exe build -force -on-error=ask -timestamp-ui $Template
