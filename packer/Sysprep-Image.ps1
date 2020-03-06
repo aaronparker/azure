cd37ac134421123c94790b38f9287995e34c64d0
Aaron Parker <aaron@stealthpuppy.com>
2020-03-06 13:35:22 +1100

    Script updates

--- packer/Sysprep-Image.ps1
+++ packer/Sysprep-Image.ps1
@@ -4,8 +4,8 @@
 #Set-Service -Name WindowsAzureGuestAgent -StartupType Disabled; Stop-Service -Name WindowsAzureGuestAgent -ErrorAction SilentlyContinue"
 #Remove-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Setup\SysPrepExternal\Generalize' -Name '*'"
 
-Write-Output \"====== Run Sysprep\""
-& $env:SystemRoot\System32\Sysprep\Sysprep.exe /oobe /generalize /quiet /quit"
+Write-Output "====== Run Sysprep"
+& $env:SystemRoot\System32\Sysprep\Sysprep.exe /oobe /generalize /quiet /quit
 
 While ($True) {
     $imageState = Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Setup\State | Select-Object ImageState
