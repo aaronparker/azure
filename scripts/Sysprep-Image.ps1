455296e6b6658107c96c0697da09d0c6e7dd414d
Aaron Parker <aaron@stealthpuppy.com>
2020-05-13 08:32:12 +1000

    Update Sysprep script

--- scripts/Sysprep-Image.ps1
+++ scripts/Sysprep-Image.ps1
@@ -4,6 +4,17 @@
 #Set-Service -Name WindowsAzureGuestAgent -StartupType Disabled; Stop-Service -Name WindowsAzureGuestAgent -ErrorAction SilentlyContinue"
 #Remove-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Setup\SysPrepExternal\Generalize' -Name '*'"
 
+# Wait for services
+If (Get-Service -Name RdAgent -ErrorAction SilentlyContinue) {
+    while ((Get-Service -Name RdAgent).Status -ne 'Running') { Start-Sleep -s 5 }
+}
+If (Get-Service -Name WindowsAzureTelemetryService -ErrorAction SilentlyContinue) {
+    while ((Get-Service -Name WindowsAzureTelemetryService).Status -ne 'Running') { Start-Sleep -s 5 }
+}
+If (Get-Service -Name WindowsAzureGuestAgent -ErrorAction SilentlyContinue) {
+    while ((Get-Service -Name WindowsAzureGuestAgent).Status -ne 'Running') { Start-Sleep -s 5 }
+}
+
 Write-Output "====== Run Sysprep"
 & $env:SystemRoot\System32\Sysprep\Sysprep.exe /oobe /generalize /quiet /quit
 
