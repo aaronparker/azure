f68d0eaf24137021a81a5c6f7f5fe3e083798013
Aaron Parker <aaron@stealthpuppy.com>
2020-11-12 14:41:28 +1100

    Bicep; script updates

--- azure/infrastructure/New-Deployment.ps1
+++
@@ -1,4 +0,0 @@
-az account set --subscription 63e8f660-f6a4-4ac5-ad4e-623268509f20
-az deployment group create --resource-group "rg-HubNetworkInfrastructure-AustraliaSoutheast" \
-    --template-file ./update-management.json \
-    --parameters ./update-management.parameters.json
---
+++ azure/infrastructure/New-Deployment.sh
@@ -0,0 +1,11 @@
+az login
+az account set --subscription 63e8f660-f6a4-4ac5-ad4e-623268509f20
+
+az deployment group create --resource-group "rg-HubNetworkInfrastructure-AustraliaSoutheast" \
+    --template-file ./update-management.json \
+    --parameters ./update-management.parameters.json
+
+
+az deployment group create --resource-group "rg-WindowsVirtualDesktopInfrastructure-AustraliaEast" \
+    --template-file /Users/aaron/Projects/build-azure/azure/bicep/wvd-workspace.json \
+    --parameters /Users/aaron/Projects/build-azure/azure/bicep/wvd-workspace.parameters.json
