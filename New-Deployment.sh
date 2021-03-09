fec44624a95b76f4efe64bdf49a3d63bae20c54c
Aaron Parker <aaron@stealthpuppy.com>
2021-03-10 10:34:04 +1100

    WVD templates

--- New-Deployment.sh
+++ New-Deployment.sh
@@ -1,4 +1,4 @@
-az login
+az login --use-device-code
 az account set --subscription 63e8f660-f6a4-4ac5-ad4e-623268509f20
 
 az deployment group create --resource-group "rg-HubNetworkInfrastructure-AustraliaSoutheast" \
@@ -7,11 +7,11 @@ az deployment group create --resource-group "rg-HubNetworkInfrastructure-Austral
 
 
 az deployment group create --resource-group "rg-WindowsVirtualDesktopInfrastructure-AustraliaEast" \
-    --template-file /Users/aaron/Projects/build-azure/azure/bicep/wvd-workspace.json \
-    --parameters /Users/aaron/Projects/build-azure/azure/bicep/wvd-workspace.parameters.json
+    --template-file /Users/aaron/Projects/azure/bicep/wvd-workspace.json \
+    --parameters /Users/aaron/Projects/azure/bicep/wvd-workspace.parameters.json
 
 
 az deployment group create --resource-group "rg-WindowsVirtualDesktopPersonal-AustraliaEast" \
-    --template-file /Users/aaron/Projects/build-azure/azure/wvd/personal-hostpool/template.json \
-    --parameters /Users/aaron/Projects/build-azure/azure/wvd/personal-hostpool/parameters.json
+    --template-file /Users/aaron/Projects/azure/wvd/personal-hostpool/template.json \
+    --parameters /Users/aaron/Projects/azure/wvd/personal-hostpool/parameters.json
 
